d50be9b2ffd2613476f9589a87b86a3a26cded13
Justin Navarro <info@justinnavarro.net>
2023-04-15 11:00:32 -0400

    revert the app service provider and separated it's contents to their prospective service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use Filament\Facades\Filament;
-use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
-use JeffGreco13\FilamentBreezy\FilamentBreezy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,21 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        FilamentBreezy::setPasswordRules(
-            [
-                Password::min(8)
-                    ->letters()
-                    ->numbers()
-                    ->mixedCase()
-                    ->symbols()
-                    ->uncompromised(3)
-            ]
-        );
-
-        Filament::serving(function () {
-            Filament::registerTheme(
-                app(Vite::class)('resources/css/app.css'),
-            );
-        });
+        //
     }
 }
