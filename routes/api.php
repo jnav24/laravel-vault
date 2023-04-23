a372435285e33b920efb867cb562a5d6295f8838
Justin Navarro <info@justinnavarro.net>
2023-04-23 10:31:23 -0400

    created routes for the mfa api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAccessController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/generate', [UserAccessController::class, 'store'])->name('api.generate');
+Route::post('/verify/{token}/site/{app_key}', [UserAccessController::class, 'verify'])->name('api.verify');
+Route::delete('/delete/{token}', [UserAccessController::class, 'destroy'])->name('api.delete');
