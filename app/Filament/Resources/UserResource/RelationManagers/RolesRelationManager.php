85f6b41f1345d7d738d44869873c9168942eaa8d
Justin Navarro <info@justinnavarro.net>
2023-04-25 04:46:17 -0400

    role relations manager

---
+++ app/Filament/Resources/UserResource/RelationManagers/RolesRelationManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Resources\PermissionResource;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RolesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'roles';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return PermissionResource::table($table);
+    }
+}
