8aa629592800884876322a78b8302969775cf57a
Justin Navarro <info@justinnavarro.net>
2023-04-27 05:12:03 -0400

    update the routes

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/generate', [UserAccessController::class, 'store'])->name('api.generate');
 Route::post('/verify/{token}/site/{app_key}', [UserAccessController::class, 'verify'])->name('api.verify');
-Route::delete('/delete/{token}', [UserAccessController::class, 'destroy'])->name('api.delete');
+Route::delete('/delete/{token}/site/{app_key}', [UserAccessController::class, 'destroy'])->name('api.delete');
